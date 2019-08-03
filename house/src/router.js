3d4f0d10d54b4b017a2e5b2f24c35fc0fdf2a9df
yumaolin513 <939596238@qq.com>
2019-08-03 14:56:00 +0800

    pp

--- house/src/router.js
+++ house/src/router.js
@@ -5,6 +5,12 @@ import home from "./pages/home.vue"
 import GosHouse from "./pages/GosHouse.vue"
 
 import Details from "./components/Details.vue"
+
+import searchhouse from "./pages/searchhouse.vue"
+
+import getHotHouse from "./pages/getHotHouse.vue"
+
+import getHotHousedetaile from "./components/getHotHousedetaile.vue"
 import HouseGuide from "./pages/HouseGuide.vue"
 import PictureDetails from "./pages/PictureDetails.vue"
 const router= new VueRouter({
@@ -12,6 +18,15 @@ const router= new VueRouter({
   {path:"/",redirect:"home"},
   {path:"/home",component:home},
   {path:"/GosHouse/:id",component:GosHouse},
+  {path:"/GosHouse/:englishcountry/:region_unique_name:/school_unique_name",component:GosHouse},
+  {path:"/Details/:id",component:Details},
+  {path:"/searchhouse",component:searchhouse},
+  {path:"/getHotHouse",component:getHotHouse,
+   children:[{
+    path:"getHotHousedetaile",component:getHotHousedetaile
+   }]
+  },
+
   {path:"/Details/:id",component:Details},
   {path:"/HouseGuide/:id",component:HouseGuide},
   {path:"/PictureDetails/:id",component:PictureDetails},
